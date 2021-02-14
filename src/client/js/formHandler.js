2c004d8b802eab7212316d370582d1325fda8260
edicostasp <64762246+edicostasp@users.noreply.github.com>
2021-02-14 15:28:15 -0300

    Lots of adjustments

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -5,12 +5,49 @@ function handleSubmit(event) {
     let formText = document.getElementById('name').value
     checkForName(formText)
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    if(Client.checkForURL(formText)) {
+        console.log("::: Form Submitted :::")
+
+        postData('http://localhost:8081/api', {url: formText})
+
+        //fetch('http://localhost:8080/test')
+        .then(res => res.json())
+        .then(function(res) {
+            document.getElementById('polarity').innerHTML = 'Polarity: '+polarityChecker(res.score_tag);
+            document.getElementById("agreement").innerHTML = `Agreement: ${res.agreement}`;
+            document.getElementById("subjectivity").innerHTML = `Subjectivity: ${res.subjectivity}`;
+            document.getElementById("confidence").innerHTML = `Confidence: ${res.confidence}`;
+            document.getElementById("irony").innerHTML = `Irony: ${res.irony}`;
+        })
+    } else {
+        alert('Seems like an invalid URL, please try with a valid URL.');
+    }
+}
+
+// API response output (https://www.meaningcloud.com/developer/sentiment-analysis/doc/2.1/response)
+const polarityChecker = (score) => {
+    let display;
+    switch (score){
+        case 'P+':
+            display = 'strong positive';
+            break;
+        case 'P':
+            display = 'positive';
+            break;
+        case 'NEW':
+            display = 'neutral';
+            break;
+        case 'N':
+            display = 'negative';
+            break;
+        case 'N+':
+            display = 'strong negative';
+            break;
+        case 'NONE':
+            display = 'no sentiment';
+    }
+    return display.toUpperCase();
 }
 
 export { handleSubmit }
+export { polarityChecker }
--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,33 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+// Start up an instance of app
 const app = express()
 
+// Cors allows the browser and server to communicate without any security interruptions
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
 app.use(express.static('dist'))
 
 console.log(__dirname)
 
+// API
+const baseURL = 'https://api.meaningcloud.com/sentiment-2.1?'
+const apiKey = process.env.API_KEY
+console.log(`Your API Key is ${process.env.API_KEY}`);
+let userInput = []
+
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
@@ -20,4 +37,15 @@ app.listen(8080, function () {
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+})
+
+// POST Route
+app.post('/api', async function(req, res) {
+    userInput = req.body.url;
+    console.log(`You entered: ${userInput}`);
+    const apiURL = `${baseURL}key=${apiKey}&url=${userInput}&lang=en`
+    const response = await fetch(apiURL)
+    const mcData = await response.json()
+    res.send(mcData)
+    console.log(mcData)
 })
