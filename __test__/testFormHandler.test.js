84711826ce97cc3f6038b5c7c44e641566b2b1be
edicostasp <64762246+edicostasp@users.noreply.github.com>
2021-03-11 20:39:04 -0300

    Changed a variable and import a function

--- __test__/testFormHandler.test.js
+++ __test__/testFormHandler.test.js
@@ -1,6 +1,6 @@
 // Import the js file to test
 import { handleSubmit } from '../src/client/js/formHandler';
-
+import { checkAgreement } from '../src/client/js/formHandler';
 // The describe() function takes two arguments - a string description, and a test suite as a callback function.  
 // A test suite may contain one or more related tests    
 describe("Testing the submit functionality", () => {
@@ -20,7 +20,7 @@ describe("Testing the submit functionality", () => {
 // Testing the check agreement result
 describe ('Testing result of agreement', () => {
     test('result must be agreement', () => {
-        const data = 'AGREEMENT';
+        const data = "AGREEMENT";
         const response = "agreement!! (different elements have exactly the same polarity)";
         expect(checkAgreement(data)).toEqual(response);
     });
@@ -29,7 +29,7 @@ describe ('Testing result of agreement', () => {
 // Testing the check disagreement result
 describe ('Testing result of agreement', () => {
     test('result must be disagreement', () => {
-        const data = 'DISAGREEMENT';
+        const data = "DISAGREEMENT";
         const response = "disagreement!! (there is disagreement between different elements polarity)";
         expect(checkAgreement(data)).toEqual(response);
     });
--- __test__/testUrlChecker.test.js
+++ __test__/testUrlChecker.test.js
@@ -4,17 +4,32 @@ import { checkForURL } from "../src/client/js/urlChecker"
 //Testing a valid URL
 describe("Check a valid URL", () => {
   test('Testing a valid URL', () => {
-      const inputUrl = 'http://www.udacity.com';
-      const response = 1;
+      const inputUrl = 'https://www.udacity.com';
+      const response = true;
       expect(checkForUrl(inputUrl)).toEqual(response);
+  });
+  test('Testing a valid URL', () => {
+    const inputUrl = 'http://www.udacity.com';
+    const response = true;
+    expect(checkForUrl(inputUrl)).toEqual(response);
   });
 });
 
 //Testing an invalid URL
 describe("Check an invalid URL", () => {
   test('Testing an invalid URL', () => {
-      const inputUrl = 'udacity.com';
-      const response = 0;
+      const inputUrl = 'www.udacity.com';
+      const response = false;
       expect(checkForUrl(inputUrl)).toEqual(response);
+  });
+  test('Testing an invalid URL', () => {
+    const inputUrl = 'udacity.com';
+    const response = false;
+    expect(checkForUrl(inputUrl)).toEqual(response);
+  });
+  test('Testing an invalid URL', () => {
+    const inputUrl = 'udacity . com';
+    const response = false;
+    expect(checkForUrl(inputUrl)).toEqual(response);
   });
 });
