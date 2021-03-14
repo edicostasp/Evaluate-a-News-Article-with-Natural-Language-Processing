b27e02b33ab510cecfc9a6df60c4aaeed021df35
edicostasp <64762246+edicostasp@users.noreply.github.com>
2021-03-14 16:16:46 -0300

    Test Adjustments

--- __test__/testUrlChecker.test.js
+++ __test__/testUrlChecker.test.js
@@ -1,17 +1,16 @@
 // Import the js file to test
 import { checkForURL } from "../src/client/js/urlChecker"
-
 //Testing a valid URL
 describe("Check a valid URL", () => {
   test('Testing a valid URL', () => {
       const inputUrl = 'https://www.udacity.com';
       const response = true;
-      expect(checkForUrl(inputUrl)).toEqual(response);
+      expect(checkForURL(inputUrl)).toEqual(response);
   });
   test('Testing a valid URL', () => {
     const inputUrl = 'http://www.udacity.com';
     const response = true;
-    expect(checkForUrl(inputUrl)).toEqual(response);
+    expect(checkForURL(inputUrl)).toEqual(response);
   });
 });
 
@@ -20,16 +19,16 @@ describe("Check an invalid URL", () => {
   test('Testing an invalid URL', () => {
       const inputUrl = 'www.udacity.com';
       const response = false;
-      expect(checkForUrl(inputUrl)).toEqual(response);
+      expect(checkForURL(inputUrl)).toEqual(response);
   });
   test('Testing an invalid URL', () => {
     const inputUrl = 'udacity.com';
     const response = false;
-    expect(checkForUrl(inputUrl)).toEqual(response);
+    expect(checkForURL(inputUrl)).toEqual(response);
   });
   test('Testing an invalid URL', () => {
     const inputUrl = 'udacity . com';
     const response = false;
-    expect(checkForUrl(inputUrl)).toEqual(response);
+    expect(checkForURL(inputUrl)).toEqual(response);
   });
 });
